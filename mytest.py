dba32d77ee0ef46bcf990c5db0cad2707d65cbd9
UltralyticsAssistant <web@ultralytics.com>
2026-05-19 14:30:22 +0000

    Auto-format by https://ultralytics.com/actions

--- mytest.py
+++ mytest.py
@@ -1,11 +1,9 @@
 from ultralytics import YOLO
+
 model = YOLO(r"yolov8n-pose.pt")
 
 model.predict(
-
-source=r"pose_test1.mp4",
+    source=r"pose_test1.mp4",
     save=False,
     show=True,
-
-
 )
